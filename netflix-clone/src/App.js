e1ed88b92d1cbc6c2f5591f98032bf5093c25a23
Bora Fatih KAZANCI <fatih.kazanci@ug.bilkent.edu.tr>
2022-02-10 15:10:51 +0300

    Directs login page on start.

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import SigninPage from "./pages/SigninPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<SigninPage/>}/>
+        <Route path="/login" element={<SigninPage />} />
+        <Route path="/" element={<Navigate replace to="/login" />} />
       </Routes>
     </Router>
   );
