938f1f7377bde9d309ea484606c27407af9890b8
Mert Yıldırım <yusuf.yildirim@ug.bilkent.edu.tr>
2022-02-26 14:19:45 +0300

    Implemented Test Cases

--- test_cases/test_case_3.py
+++ test_cases/test_case_3.py
@@ -9,7 +9,7 @@ import os
 Case#1: Verify if the font, text color, and color coding of the Login page is as per the standard.
 """
 
-class TestStringMethods(unittest.TestCase):
+class TestLogoProperties(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -19,33 +19,60 @@ class TestStringMethods(unittest.TestCase):
 
     def test(self):
         time.sleep(2)
-        email_box = self.driver.find_element(By.ID, "inputEmail")
-        email_box.send_keys('bobross@outlook.com')
 
-        # write password of the user
-        time.sleep(2)
-        password_box = self.driver.find_element(By.ID, "inputPassword")
-        password_box.send_keys('test')
-
-        # click sign button
-        time.sleep(2)
-        sign_button = self.driver.find_element(By.ID, "sign-button")
-        sign_button.click()
-        time.sleep(3)
-
-        success_text = self.driver.find_element(By.ID, "success-login-text")
-        success_text = success_text.get_attribute("innerHTML")
-
-        if success_text == "Successfully logged in!":
+        logo = self.driver.find_element(By.CLASS_NAME, "img-logo")
+        logo_height = logo.value_of_css_property("height")
+        logo_margin = logo.value_of_css_property("margin-left")
+        if logo_height == "94px" and logo_margin == "45px":
             testValue = True
         else:
             testValue = False
-        # error message in case if test case got failed
-        message = "Test value is not true."
-        # assertTrue() to check true of test value
-        self.assertTrue(testValue, message)
+        
+        self.assertTrue(testValue, "Test value is not true.")
         self.driver.quit()
 
+class TestLoginTextProperties(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(
+            'file:///' + os.path.dirname(os.getcwd()) + '/index.html')
+        self.driver.maximize_window()
+
+    def test(self):
+        otac = self.driver.find_element(By.ID, "otac")
+        otac_color = otac.value_of_css_property("color")
+        otac_padding = otac.value_of_css_property("padding-bottom")
+        otac_font_size = otac.value_of_css_property("font-size")
+        otac_font_weight = otac.value_of_css_property("font-weight")
+        otac_font = otac.value_of_css_property("font-family")
+        if otac_color == "rgba(255, 255, 255, 1)" and otac_padding == "10px" and otac_font_size == "32px" and otac_font_weight == "700" and otac_font == '"Helvetica Neue", Helvetica, Arial, sans-serif':
+            testValue = True
+        else:
+            testValue = False
+        
+        self.assertTrue(testValue, "Test value is not true.")
+        self.driver.quit()
+
+class TestLoginButtonProperties(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(
+            'file:///' + os.path.dirname(os.getcwd()) + '/index.html')
+        self.driver.maximize_window()
+
+    def test(self):
+        sign_in_button = self.driver.find_element(By.ID, "sign-button")
+        sign_in_button_background_color = sign_in_button.value_of_css_property("background-color")
+        sign_in_button_color = sign_in_button.value_of_css_property("color")
+        sign_in_button_font_size = sign_in_button.value_of_css_property("font-size")
+        sign_in_button_border_radius = sign_in_button.value_of_css_property("border-radius")
+        if sign_in_button_background_color == "rgba(255, 0, 0, 1)" and sign_in_button_color == "rgba(255, 255, 255, 1)" and sign_in_button_font_size == "16.8px" and sign_in_button_border_radius == "5px":
+            testValue = True
+        else:
+            testValue = False
+        
+        self.assertTrue(testValue, "Test value is not true.")
+        self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
