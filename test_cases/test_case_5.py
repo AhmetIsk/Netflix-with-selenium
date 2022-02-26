84087d1c38c59adb1877ae1ce6bef9f59f72c2a7
Bora Fatih KAZANCI <fatih.kazanci@ug.bilkent.edu.tr>
2022-02-26 15:59:05 +0300

    Update test_case_5.py

--- test_cases/test_case_5.py
+++ test_cases/test_case_5.py
@@ -6,12 +6,11 @@ import time
 import os
 
 """
-Case#1: Verify that all the labels and controls including text-boxes, buttons, and links are present on the Login page.
-Case#2: Check if the password is in masked form when typed in the password field.
+Case#1: Check if the password is in masked form when typed in the password field.
 """
 
 
-class TestStringMethods(unittest.TestCase):
+class TestPasswordMethods(unittest.TestCase):
 
     def setUp(self):
         options = webdriver.ChromeOptions()
@@ -22,31 +21,22 @@ class TestStringMethods(unittest.TestCase):
         self.driver.maximize_window()
 
     def test(self):
-        time.sleep(2)
-        email_box = self.driver.find_element(By.ID, "inputEmail")
-        email_box.send_keys('bobross@outlook.com')
-
-        # write password of the user
+        print('Case#2')
+        # look for password box properties
         time.sleep(2)
         password_box = self.driver.find_element(By.ID, "inputPassword")
-        password_box.send_keys('test')
+        password_box.get_attribute('type')
+        print('password_box')
+        print(password_box)
+        print('password_box')
 
-        # click sign button
-        time.sleep(2)
-        sign_button = self.driver.find_element(By.ID, "sign-button")
-        sign_button.click()
         time.sleep(3)
 
-        success_text = self.driver.find_element(By.ID, "success-login-text")
-        success_text = success_text.get_attribute("innerHTML")
-
-        if success_text == "Successfully logged in!":
+        if password_box == "":
             testValue = True
         else:
             testValue = False
-        # error message in case if test case got failed
         message = "Test value is not true."
-        # assertTrue() to check true of test value
         self.assertTrue(testValue, message)
         self.driver.quit()
 
